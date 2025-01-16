c1083842ae0a09a50ab0cd9166814f41108b171a
phamjhanhj <22010503@st.phenikaa-uni.edu.vn>
2025-01-16 14:13:49 +0700

    delete

--- thoi-khoa-bieu/database/seeders/DatabaseSeeder.php
+++ thoi-khoa-bieu/database/seeders/DatabaseSeeder.php
@@ -10,8 +10,12 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run()
+    public function run(): void
     {
-        $this->call(AccountsSeeder::class);
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
